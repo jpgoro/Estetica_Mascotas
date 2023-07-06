839dfac5cb2e06280bd648624caf880e5b88d85b
Juan Gorosito <jpablo.gorosito87@gmail.com>
2023-07-06 01:33:52 -0300

    Agregando evento btn Salir

--- src/main/java/com/mycompany/pelucanina/igu/Principal.java
+++ src/main/java/com/mycompany/pelucanina/igu/Principal.java
@@ -122,7 +122,7 @@ public class Principal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+        this.dispose();   // TODO add your handling code here:
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void btnCargaDatosActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCargaDatosActionPerformed
